b991431b1e61953430a566a3f29b2fcae779e6e5
rizkyaziz2002 <rizkyaziz2002@gmail.com>
2019-08-06 20:43:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/inputuser/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.inputuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText bil1;
+    private EditText bil2;
+    private EditText jumlah;
+    private Button btnhasil;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        initEvent();
+    }
+
+    private void initUI() {
+        bil1 = (EditText) findViewById(R.id.edittext1);
+        bil2 = (EditText) findViewById(R.id.edittext2);
+        jumlah = (EditText) findViewById(R.id.EtxtHasil);
+        btnhasil = (Button) findViewById(R.id.btn);
+    }
+
+    private void initEvent() {
+        btnhasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hitungJumlah();
+            }
+        });
+    }
+
+    private void hitungJumlah() {
+        int angka1 = Integer.parseInt(bil1.getText().toString());
+        int angka2 = Integer.parseInt(bil2.getText().toString());
+        int total = angka1 + angka2;
+        jumlah.setText(total+"");
+    }
+}
